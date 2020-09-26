c0d13a221cb8577988e33d55b4a56b3f65f84bb2
noypearl <60noypearl@gmail.com>
2020-09-26 19:27:18 +0300

    improved error status assignment

--- middlewares.js
+++ middlewares.js
@@ -6,13 +6,16 @@ module.exports = {
     tokenMiddleware : (req, res, next) => {
         const token = req.get('Token')
         if (!token) {
-        logger.error(`No token was provided in middleware to ${req.url}`)``
-            res.statusCode = 401;
-            return next(new Error("No token was provided"));
+        logger.error(`No token was provided in middleware to ${req.url}`)
+        const err = new Error("No token was provided");
+        err.status - 401;
+        return next(err);
         }
         if (!validator.isJWT(token)){
+            const err = new Error("Nope. Try again");
+            err.status - 401;
             logger.error(`Invalid input was provided instead of JWT token to middleware in ${req.url}. Token: ${token}`)
-            return next(new Error("Nope. Try again"));
+            return next(err);
         }
         const { url } = req;
         logger.info(`middleware valid token to ${url}. Token: ${token}`)
